1804483a4f92a735b8a3e63ca98289ca7bc402ae
Zaid Akhter <zaidakhter1202@gmail.com>
2020-10-02 20:57:30 +0500

    📦 NEW: Add graphql-request

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { request, gql } from "graphql-request";
 
 function App() {
+  const query = gql`
+    {
+      _
+    }
+  `;
+
+  useEffect(() => {
+    request("http://localhost:4000", query).then((data) => console.log(data));
+  }, [query]);
+
   return (
     <div className="app">
       <h1>Upload File</h1>
